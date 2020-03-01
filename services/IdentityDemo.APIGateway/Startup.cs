3b6f048138f50ceebd51a38cba3641eccebb11b4
daxnet <daxnet@outlook.com>
2020-03-01 22:21:19 +0800

    Added source code for chapter 2.

---
+++ services/IdentityDemo.APIGateway/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace IdentityDemo.APIGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+            services.AddAuthentication()
+                .AddIdentityServerAuthentication("AuthKey", options =>
+                {
+                    options.Authority = "http://localhost:7889";
+                    options.RequireHttpsMetadata = false;
+                });
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()));
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseOcelot();
+            app.UseCors("AllowAll");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
---
+++ services/IdentityDemo.StandaloneConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.StandaloneConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            var discoResponse = await client.GetDiscoveryDocumentAsync("https://localhost:7890");
+            if (discoResponse.IsError)
+            {
+                Console.WriteLine(discoResponse.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = discoResponse.TokenEndpoint,
+                ClientId = "webapi",
+                Scope = "api.weather.full_access",
+                ClientSecret = "mysecret",
+                UserName = "daxnet",
+                Password = "P@ssw0rd123"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:9000/api/weather");
+            Console.WriteLine(response.IsSuccessStatusCode ?
+                $"{response.StatusCode} {await response.Content.ReadAsStringAsync()}" : 
+                response.StatusCode.ToString());
+        }
+    }
+}
